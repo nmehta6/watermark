db085591389623e293eefe77e87166eb1fbad91a
nachiket mehta <nmehta@mediafly.com>
2015-08-04 15:12:08 -0500

    play video on canvas

--- src/home/homeController.js
+++ src/home/homeController.js
@@ -1,3 +1,20 @@
 angular.module('app').controller('homeController', function() {
+	var v = document.getElementById('videoelement');
+	var canvas = document.getElementById('videocanvas');
+	var context = canvas.getContext('2d');
 
+	var cw = canvas.clientWidth;
+	var ch = canvas.clientHeight;
+
+	v.addEventListener('play', function() {
+		draw(this, context, cw, ch);
+	}, false);
+
+	function draw(v, c, w, h) {
+		if (v.paused || v.ended) {
+			return false
+		}
+		c.drawImage(v, 0, 0, w, h);
+		setTimeout(draw, 20, v, c, w, h);
+	}
 });
