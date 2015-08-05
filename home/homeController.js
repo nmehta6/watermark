dbbf25c5c1ddeb6750090a4e674f78a6a2d200a4
nachiket mehta <nmehta@mediafly.com>
2015-08-05 10:43:02 -0500

    remove black and white transform

--- home/homeController.js
+++ home/homeController.js
@@ -29,26 +29,26 @@ angular.module('app').controller('homeController', function() {
 		}
 
 		// First, draw it into the backing canvas
-		bc.drawImage(v, 0, 0, w, h);
+		c.drawImage(v, 0, 0, w, h);
 
 		// Grab the pixel data from the backing canvas
-		var idata = bc.getImageData(0,0,w,h);
-		var data = idata.data;
+		// var idata = bc.getImageData(0,0,w,h);
+		// var data = idata.data;
 
 		// Loop through the pixels, turning them grayscale
-		for(var i = 0; i < data.length; i+=4) {
-			var r = data[i];
-			var g = data[i + 1];
-			var b = data[i + 2];
-			var brightness = (3 * r + 4 * g + b) >>> 3;
-			data[i] = brightness;
-			data[i + 1] = brightness;
-			data[i + 2] = brightness;
-		}
-		idata.data = data;
+		// for(var i = 0; i < data.length; i+=4) {
+		// 	var r = data[i];
+		// 	var g = data[i + 1];
+		// 	var b = data[i + 2];
+		// 	var brightness = (3 * r + 4 * g + b) >>> 3;
+		// 	data[i] = brightness;
+		// 	data[i + 1] = brightness;
+		// 	data[i + 2] = brightness;
+		// }
+		// idata.data = data;
 
 		// Draw the pixels onto the visible canvas
-		c.putImageData(idata, 0, 0);
+		// c.putImageData(idata, 0, 0);
 		c.fillText('Watermark Demo', 58, 165);
 		c.fillText('Watermark Demo', 558, 165);
 		c.fillText('Watermark Demo', 58, 565);
