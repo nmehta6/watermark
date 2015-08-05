8b2498a504ba06d80a7f21bffd43183becc518e7
nachiket mehta <nmehta@mediafly.com>
2015-08-05 10:25:28 -0500

    revert

--- app/appRoutes.js
+++ app/appRoutes.js
@@ -1,4 +1,4 @@
 angular.module('app').config(function($locationProvider, $routeProvider) {
 	$locationProvider.html5Mode(true);
-	// $routeProvider.otherwise({redirectTo: '/watermark'});
+	$routeProvider.otherwise({redirectTo: '/'});
 });
