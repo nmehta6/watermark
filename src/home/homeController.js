5421dd93f1d8ca60cb693c952ca4a92d1beb705b
nachiket mehta <nmehta@mediafly.com>
2015-08-06 10:28:32 -0500

    remove dead code

--- src/home/homeController.js
+++ src/home/homeController.js
@@ -5,6 +5,7 @@ angular.module('app').controller('homeController', function() {
 	var videoElement = document.createElement('video');
 	videoElement.setAttribute('src', 'img/sintel-trailer.mp4');
 	videoElement.setAttribute('autoplay', 'true');
+	videoElement.setAttribute('loop', 'true');
 
 	var canvas = document.getElementById('videocanvas');
 	var context = canvas.getContext('2d');
@@ -14,8 +15,6 @@ angular.module('app').controller('homeController', function() {
 	var ch = '720';
 
 	videoElement.addEventListener('play', function() {
-		// canvas.width = cw;
-		// canvas.height = ch;
 		back.width = '1280';
 		back.height = '720';
 
@@ -28,26 +27,8 @@ angular.module('app').controller('homeController', function() {
 		}
 
 		// First, draw it into the backing canvas
-		bc.drawImage(v, 0, 0, w, h);
+		c.drawImage(v, 0, 0, w, h);
 
-		// Grab the pixel data from the backing canvas
-		var idata = bc.getImageData(0,0,w,h);
-		var data = idata.data;
-
-		// Loop through the pixels, turning them grayscale
-		for(var i = 0; i < data.length; i+=4) {
-			var r = data[i];
-			var g = data[i + 1];
-			var b = data[i + 2];
-			var brightness = (3 * r + 4 * g + b) >>> 3;
-			data[i] = brightness;
-			data[i + 1] = brightness;
-			data[i + 2] = brightness;
-		}
-		idata.data = data;
-
-		// Draw the pixels onto the visible canvas
-		c.putImageData(idata, 0, 0);
 		c.fillText('Watermark Demo', 58, 165);
 		c.fillText('Watermark Demo', 558, 165);
 		c.fillText('Watermark Demo', 58, 565);
