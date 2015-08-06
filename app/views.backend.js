5d9c9f313bf3b6f22f7e7d719aa5fdbb9bc091c4
nachiket mehta <nmehta@mediafly.com>
2015-08-06 10:33:43 -0500

    clean

--- app/views.backend.js
+++
@@ -1,3 +0,0 @@
-angular.module('app').run(function($httpBackend) {
-	return $httpBackend.whenGET(/^.*\.(html|htm)$/).passThrough();
-});
--- components/markdown/markdownService.js
+++
@@ -1,8 +0,0 @@
-angular.module('app').factory('markdownService', function($window) {
-	var converter = new $window.Showdown.converter();
-	return {
-		convert: function(content) {
-			return converter.makeHtml(content);
-		}
-	};
-});
